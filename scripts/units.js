49137c5af8cd7967da3b8539474aa6395af12f6e
CancerGuy <70016211+AzhurniyBaklan@users.noreply.github.com>
2021-01-31 22:24:03 +0500

    Add files via upload

--- scripts/units.js
+++ scripts/units.js
@@ -5,3 +5,11 @@ embarassment.defaultController = () => extend(MinerAI, {});
 const backwater = extendContent(UnitType, "backwater", {});
 backwater.constructor = () => extend(UnitEntity, {});
 backwater.defaultController = () => extend(MinerAI, {});
+
+const scarab = extendContent(UnitType, "scarab", {});
+scarab.constructor = () => extend(UnitEntity, {});
+scarab.defaultController = () => extend(MinerAI, {});
+
+const enforcer = extendContent(UnitType, "enforcer", {});
+enforcer.constructor = () => extend(UnitEntity, {});
+enforcer.defaultController = () => extend(BuilderAI, {});
